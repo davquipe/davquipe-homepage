1a7d75232ce25b1775f1211d1b313b58bd4c6bea
davquipe <davidcondori99@gmail.com>
2022-06-14 22:35:10 -0500

    Add profile image

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-// import React from 'react'
-
-import { Box, Container, Heading } from "@chakra-ui/react"
+import { Box, Container, Heading, Image, useColorModeValue } from "@chakra-ui/react"
+// import Image from "next/image"
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius='lg' bg='red' p={3} mb={6} aling='center'>
+      <Box borderRadius='lg' bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align='center'>
         Hello, I&apos;m a FrontEnd developer based in Peru!
       </Box>
       <Box display={{md: 'flex'}}>
@@ -14,6 +13,9 @@ const Page = () => {
               David Condori
             </Heading>
             <p>Digital Craftman ( Artist / Developer / Desingner )</p>
+          </Box>
+          <Box flexShrink={0} mt={{base: 4, md: 0}} ml={{md: 6}} align='center'>
+            <Image borderColor='whiteAlpha.800' borderWidth={2} borderStyle='solid' maxWidth='100px' display='inline-block' borderRadius='full' src='/images/takuya.jpg' alt="Profile Image"/>
           </Box>
       </Box>
     </Container>
