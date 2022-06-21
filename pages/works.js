9610aa432af6e7d5e39692857681ac68fc50cd93
davquipe <davidcondori99@gmail.com>
2022-06-21 10:55:53 -0500

    Add 'Works' page

---
+++ pages/works.js
@@ -0,0 +1,30 @@
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import thumbInkdrop from '../public/images/works/inkdrop_01.png'
+import thumbWalknote from '../public/images/works/walknote_eyecatch.png'
+
+const Works = () => {
+  return (
+    <Container>
+        <Heading as='h3' fontSize={20} mb={4}>
+            Works
+        </Heading>
+
+        <SimpleGrid columns={[1,1,2]} gap={6}>
+            <Section>
+                <WorkGridItem id='inkdop' title='Inkdrop' thumbnail={thumbInkdrop}>
+                    A markdown note-talking app
+                </WorkGridItem>
+            </Section>
+            <Section>
+                <WorkGridItem id='walknote' title='Walknote' thumbnail={thumbWalknote}>
+                    Music recomendation for iOS app
+                </WorkGridItem>
+            </Section>
+        </SimpleGrid>
+    </Container>
+  )
+}
+
+export default Works
